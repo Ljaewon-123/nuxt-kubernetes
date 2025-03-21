d409fb846128efcc1e8adf7e74429fb6f113714f
Ljaewon-123 <jml6388@naver.com>
2025-03-21 14:53:55 +0900

    fix: option

--- server/plugins/dbconnections.ts
+++ server/plugins/dbconnections.ts
@@ -5,12 +5,13 @@ export default async() => {
 
   const config = useRuntimeConfig()
   console.log(config.dbName, '!!!!!!')
+  console.log(config.dburl, 'db url')
 
   try{
     const dbOptions = {
       dbName: config.dbName,
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     }
     await mongoose.connect(config.dburl, dbOptions)
     console.log('connected successfully...!!!!')
