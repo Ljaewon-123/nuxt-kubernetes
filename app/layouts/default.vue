6ae6c69db1584b8ceaa2beefecacd3edd209bb15
Ljaewon-123 <jml6388@naver.com>
2025-03-21 10:52:26 +0900

    add route

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -6,6 +6,12 @@
       <NuxtLink to="login">login</NuxtLink>
       -
       <NuxtLink to="test">test</NuxtLink>
+      -
+      <NuxtLink to="compare-fetch">compare-fetch</NuxtLink>
+      -
+      <NuxtLink to="hydrate">hydrate</NuxtLink>
+      -
+      <NuxtLink to="trpc">trpc</NuxtLink>
     </nav>
     <slot></slot>
   </div>
