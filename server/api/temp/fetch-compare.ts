f1fcab72a4f75e33de274fb7379e9eb22bbd5940
Ljaewon-123 <jml6388@naver.com>
2025-03-31 16:39:19 +0900

    test

--- server/api/temp/fetch-compare.ts
+++ server/api/temp/fetch-compare.ts
@@ -1,5 +1,24 @@
+import { createStorage, prefixStorage } from "unstorage";
+
 export default defineEventHandler( async(event) => {
   const { page, otherParam } = getQuery(event)
+
+  // test some storage functions
+  // const redis = useStorage("redis")
+  // const redisPrefix = useStorage("redis:greet")
+  // redis.set('hi', 'redis')
+  // redisPrefix.set('hi', "Who is greet?")
+  // redis.set('foo:hi', 'best for gretting is hi')
+  // redisPrefix.set("foo:hi", "what you needs?")
+  
+  // This not work because reference nuxt.config.ts storage
+  // const storage = useStorage()
+  // const db = useStorage("db")
+  // storage.set('hi', 'default storage')
+  // db.set('hi', 'db')
+  // storage.set('foo:hi', 'best for gretting is hi')
+  // db.set('foo:hi', 'best for gretting is hi')
+
   return { 
     text: 'This is the example endpoint',
     page,
